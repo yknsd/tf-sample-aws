1b3818983f39b0a5f01c98b094b7eac4fd851306
yknsd <59571141+yknsd@users.noreply.github.com>
2024-10-18 00:09:54 +0900

    Update main

--- main.tf
+++ main.tf
@@ -18,35 +18,63 @@ module "iam" {
   source = "./modules/iam"
 }
 
-module "ec2" {
-  source = "./modules/ec2"
+module network {
+  source = "./modules/network"
+
+  region = var.region
 }
 
 module "s3" {
   source = "./modules/s3"
 }
 
-//module "efs" {
-//  source = "./modules/efs"
-//}
+module "ec2" {
+  source = "./modules/ec2"
+
+  key_name      = ""
+  sg_ids        = [module.network.security_group_ec2_id]
+  subnet_id     = element(module.network.private_subnet_ids, 0)
+}
+
+module "elastic_load_balancing" {
+  source = "./modules/elastic_load_balancing"
+
+  vpc_id                      = module.network.vpc_id
+  access_log_s3_bucket_id     = module.s3.s3_bucket_example_id
+  lb_listener_security_policy = var.aws_lb_listener_security_policy
+  ec2_sample_server_1_id      = module.ec2.aws_instance_sample_server_1_id
+  alb_security_group_id       = module.network.security_group_alb_id
+  alb_subnets                 = module.network.public_subnet_ids
+}
+
+module "efs" {
+  source = "./modules/efs"
+  sg_ids = [module.network.security_group_efs_id]
+}
 //
 //module "elastic_block_storage" {
 //  source = "./modules/elastic_block_storage"
 //}
 //
-//module "rds" {
-//  source = "./modules/rds"
-//
-//}
-//
+module "rds" {
+  source = "./modules/rds"
+
+}
+
 //module "elasti_cache" {
 //  source = "modules/elasti_cache"
 //}
 //
-//module "elastic_load_balancing" {
-//  source = "./modules/elastic_load_balancing"
-//}
-//
 //module "code_pipeline" {
 //  source = "./modules/code_pipeline"
+//}
+
+//module "cloud_watch" {
+//  source = "./modules/cloud_watch"
+//
+//  region = var.region
+//}
+
+//module "cloud_front" {
+//  source = "./modules/cloud_front"
 //}
--- variables.tf
+++ variables.tf
@@ -12,3 +12,8 @@ variable "secret_key" {
   type = string
   sensitive = true
 }
+
+variable "aws_lb_listener_security_policy" {
+  type = string
+  default = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+}
